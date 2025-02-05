0ada87ac4f509e90066e02e69ae3a6544cf50b0a
Harry Pierson <harry.pierson@dbos.dev>
2025-02-05 16:33:38 -0500

    more commented out code

--- src/commands/refreshDomain.ts
+++ src/commands/refreshDomain.ts
@@ -1,11 +1,11 @@
-import { logger } from '../extension';
-import type { CloudDomainNode } from '../CloudDataProvider';
+// import { logger } from '../extension';
+// import type { CloudDomainNode } from '../CloudDataProvider';
 
-export function getRefreshDomainCommand(refresh: (domain: string) => Promise<void>) {
-  return function (node?: CloudDomainNode) {
-    logger.debug("refreshDomain", { domain: node ?? null });
-    if (node) {
-      refresh(node.domain).catch(e => logger.error("refreshDomain", e));
-    }
-  };
-}
+// export function getRefreshDomainCommand(refresh: (domain: string) => Promise<void>) {
+//   return function (node?: CloudDomainNode) {
+//     logger.debug("refreshDomain", { domain: node ?? null });
+//     if (node) {
+//       refresh(node.domain).catch(e => logger.error("refreshDomain", e));
+//     }
+//   };
+// }
