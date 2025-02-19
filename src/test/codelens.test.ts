158000aa7fbd2b32cc9268206ad04431486dcf8f
Harry Pierson <harry.pierson@dbos.dev>
2025-02-19 10:48:02 -0800

    fix test import

--- src/test/codelens.test.ts
+++ src/test/codelens.test.ts
@@ -4,7 +4,7 @@ import ts from 'typescript';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-import { getImports, getStaticMethods, getWorkflowMethods, parseDecorator, StaticMethodInfo } from '../CodeLensProvider';
+import { getImports, getStaticMethods, getWorkflowMethods, parseDecorator, StaticMethodInfo } from '../parsers/typeScript';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
