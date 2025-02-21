73375de2e170acb0f54218a24660c8a08bad6ba2
Harry Pierson <harry.pierson@dbos.dev>
2025-02-21 10:00:03 -0800

    fix lint issue

--- src/CodeLensProvider.ts
+++ src/CodeLensProvider.ts
@@ -242,7 +242,7 @@ export class CodeLensProvider implements vscode.CodeLensProvider, vscode.Disposa
             }
             : undefined;
 
-        return { cloudRelay, timeTravel }
+        return { cloudRelay, timeTravel };
     }
 
     getCodeLensDebugCommand() {
